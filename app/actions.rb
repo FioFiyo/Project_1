be7d50ffd9bd46dc118cd9e1028ac3f5157cce1c
FioFiyo <fioarlego1@msn.com>
2015-11-18 18:14:52 -0800

    editing views and added css for views

--- app/actions.rb
+++ app/actions.rb
@@ -1,13 +1,16 @@
-# Homepage (Root path)
 
-#adding helper to access sinatra's sessions to identify user just created
+
+#adding helper to access sinatra's sessions to identify user just created.
+#id is specific to each user and the easiest way to get 'em for logins and creating one.
+#use instance variable so it can bee seen in views.
+# ||= means only set the parameters after = if current_user is nil at the moment.
 helpers do
   def current_user
-    @current_user = User.find_by(id: session[:user_id]) if session[:user_id]
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
 end
-
+# Homepage (Root path)
 get '/' do
   erb :index
 end
@@ -16,17 +19,36 @@ get '/login' do
   erb :login
 end
 
+get '/signup' do
+  erb :signup
+end
+
+get '/profile' do
+  erb :profile
+end
+
+
+# Logout
+get '/logout' do 
+  session[:user_id] = nil
+  redirect '/'
+  #put nil or session.clear
+end
+
+#setting up user; set up the variables first.
 post '/signup' do
   username = params[:username]
   password = params[:password]
   email =    params[:email]
 
-  email = User.find_by(email: email)
+  # user = User.new (username: username, password: password, email: email)
+
+  email = User.find_by(email: email)#wouldn't i use params[:email] to find it?
   username = User.find_by(username: username)
   if (user.email == email) || (user.username == username)
     redirect '/login'
   else 
-    user= user.create(username:username, password:password, email:email)
+    user= User.create(username:username, password:password, email:email)
     session[:user_id] = user.id
     redirect '/profile'
   end
